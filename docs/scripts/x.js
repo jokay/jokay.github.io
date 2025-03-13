25f77892a7431b3ea6ccbaf9faa1831a7ab3fdce
D. Domig <x@jokay.ch>
2025-03-13 11:19:05 +0100

    Fix linting issues

--- docs/scripts/x.js
+++ docs/scripts/x.js
@@ -1,41 +1,41 @@
-document.oncontextmenu = document.body.oncontextmenu = () => false;
+document.oncontextmenu = document.body.oncontextmenu = () => false
 
-const q = document.getElementById('m');
-const w = q.getContext('2d');
+const q = document.getElementById('m')
+const w = q.getContext('2d')
 
-q.width = window.innerWidth;
-q.height = window.innerHeight;
+q.width = window.innerWidth
+q.height = window.innerHeight
 
-let e = ' ';
+let e = ' '
 for (let r = 0; r < 45; r++) {
-  e += String.fromCharCode(r + 65393);
+  e += String.fromCharCode(r + 65393)
 }
-e = e.split('');
+e = e.split('')
 
 const t = Array(Math.floor(q.width / 16))
   .fill()
-  .map(() => Math.floor(Math.random() * -q.height / 16));
+  .map(() => Math.floor(Math.random() * -q.height / 16))
 
-let frameCount = 0;
-const frameDelay = 5;
+let frameCount = 0
+const frameDelay = 5
 
-function y() {
+function y () {
   if (++frameCount % frameDelay === 0) {
-    w.fillStyle = 'rgba(0,0,0,0.05)';
-    w.fillRect(0, 0, q.width, q.height);
-    w.fillStyle = '#0f0';
-    w.font = '16px Arial';
+    w.fillStyle = 'rgba(0,0,0,0.05)'
+    w.fillRect(0, 0, q.width, q.height)
+    w.fillStyle = '#0f0'
+    w.font = '16px Arial'
 
     t.forEach((u, i) => {
-      w.fillText(e[Math.floor(Math.random() * e.length)], i * 16, u * 16);
+      w.fillText(e[Math.floor(Math.random() * e.length)], i * 16, u * 16)
       t[i] =
         u * 16 > q.height && Math.random() > 0.975
           ? Math.floor(Math.random() * -q.height / 16)
-          : u + 1;
-    });
+          : u + 1
+    })
   }
 
-  window.requestAnimationFrame(y);
+  window.requestAnimationFrame(y)
 }
 
-y();
+y()
