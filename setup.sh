2fb38bffe48fda19b8e86f34c1e1f6a6494ed630
D. Domig <x@jokay.ch>
2026-05-16 18:32:35 +0200

    chore: fix linting issue

--- setup.sh
+++ setup.sh
@@ -5,8 +5,8 @@ set -e
 COG_VERSION="7.0.0"
 TMP_DIR=$(mktemp -d)
 
-curl -L "https://github.com/cocogitto/cocogitto/releases/download/${COG_VERSION}/cocogitto-${COG_VERSION}-x86_64-unknown-linux-musl.tar.gz" \
-    | tar -xz -C "$TMP_DIR"
+curl -L "https://github.com/cocogitto/cocogitto/releases/download/${COG_VERSION}/cocogitto-${COG_VERSION}-x86_64-unknown-linux-musl.tar.gz" |
+    tar -xz -C "$TMP_DIR"
 
 # Find the binary wherever it landed
 sudo install -m 755 "$(find "$TMP_DIR" -name 'cog' -type f)" /usr/local/bin/cog
